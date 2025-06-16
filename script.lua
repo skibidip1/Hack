b5d3a35844b67215d0742d9dbb8d0d05d96d211b
skibidip1 <vgg8862@gmail.com>
2025-06-16 10:11:58 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,15 @@
+local HttpService = game:GetService("HttpService")
 local placeId = game.PlaceId
+local UniverseID = nil
 
-if placeId == 116495829188952 then
+pcall(function()
+    UniverseID = HttpService:JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..placeId.."/universe")).universeId
+end)
+
+if UniverseID == 7018190066 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/main/deadrails"))()
-elseif placeId == 126884695634066 then
+elseif UniverseID == 7436755782 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/main/Growagarden"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/main/default"))()
 end
