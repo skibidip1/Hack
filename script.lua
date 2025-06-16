8d7749154bbd64179c77aa5aa8e901378790dbfe
skibidip1 <vgg8862@gmail.com>
2025-06-16 09:51:49 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 local place = game.PlaceId
 
 if place == 116495829188952 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/refs/heads/main/deadrails"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/main/deadrails"))()
 elseif place == 126884695634066 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/refs/heads/main/Growagarden"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/main/Growagarden"))()
 end
