edd8d9df4e0ca1e002f257ee693d5750c9ac0306
skibidip1 <vgg8862@gmail.com>
2025-09-24 19:06:11 +0700

    Update BaconHub.lua

--- BaconHub.lua
+++ BaconHub.lua
@@ -4,4 +4,6 @@ if placeId == 79546208627805 or placeId 126509999114328 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/refs/heads/main/nightintheforest.lua"))()
 elseif placeId == 116495829188952 then
 
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/refs/heads/main/fishit.lua"))()
+end
